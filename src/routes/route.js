b3464d9f3446dbff19ac69b086e7bd5144946300
Dipali127 <dipalibohara500@gmail.com>
2022-08-09 02:51:57 -0600

    Some api's problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -37,11 +37,68 @@ router.get('/test-me', function (req, res) {
      console.log(finalresult4);
     res.send('My All program run successfully!')
 });
-
+//api assignment-1:
+router.get('/movies', function(req,res)
+{
+    let movies=["Rang de basanti","The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies);
+});
+//api assignment-2 and 3rd:
+router.get('/movies/:indexNumber',function(req,res){//indexNumber is present inside req.params
+    let movies=["Rang de basanti","The shining", "Lord of the rings", "Batman begins"]
+    let number=req.params.indexNumber;//req.param present inside the above url path in router.get.
+    if(number<0 || number>=movies){
+        res.send("length exceeded")
+    }
+     res.send(movies[number]);
+});
+//api assignment -4
+router.get('/films',function(req,res){
+    const films=[{
+        "id":1,
+        "name":"The Shining"
+    },{
+        "id":2,
+        "name":"Incendies"
+    },{
+        "id":3,
+        "name":"Rang de Basanti"
+    },{
+        "id":4,
+        "name":"Finding Memo"
+    }]
+    res.send(films);
+})
+//api assignment-5
+router.get('/films/:filmId',function(req,res){
+    const films=[{
+        "id":1,
+        "name":"The Shining"
+    },{
+        "id":2,
+        "name":"Incendies"
+    },{
+        "id":3,
+        "name":"Rang de Basanti"
+    },{
+        "id":4,
+        "name":"Finding Memo"
+    }]
+    let idoffilm=req.params.filmId;
+    for(let i=0;i<films.length;i++)
+    {
+        let film=films[i]
+       if(film.id==idoffilm)
+       {
+        return res.send(film)
+       }
+    }
+    res.send("The film id doesn't match any movie");
+});
 
 router.get('/test-you', function (req, res) {
     res.send('This is the second routes implementation')
-})
+});
 
 router.get('/give-me-students-data', function (req, res) {
 
