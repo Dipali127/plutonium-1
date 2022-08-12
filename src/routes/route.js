da9d04ecbb1a7d08b82fb83d69d52fbd5589bdde
Dipali127 <dipalibohara500@gmail.com>
2022-08-12 12:38:02 -0600

    New post API

--- src/routes/route.js
+++ src/routes/route.js
@@ -28,7 +28,7 @@ router.get('/test-me', function (req, res) {
      console.log(finalresult2);
      //union function of lodash
      let array1=[0,1,2,3,4];
-     let array2=[5,6,7,8,9];
+     let array2=[4,6,7,8,9];
      let finalresult3=lodash.union(array1,array2);
      console.log(finalresult3);
      //frompairs function of lodash
@@ -47,7 +47,7 @@ router.get('/movies', function(req,res)
 router.get('/movies/:indexNumber',function(req,res){//indexNumber is present inside req.params
     let movies=["Rang de basanti","The shining", "Lord of the rings", "Batman begins"]
     let number=req.params.indexNumber;//req.param present inside the above url path in router.get.
-    if(number<0 || number>=movies){
+    if(number<0 || number>=movies.length){
         res.send("length exceeded")
     }
      res.send(movies[number]);
@@ -102,11 +102,11 @@ router.get('/films/:filmId',function(req,res){
     let SumWithoutMissingNo=0;
     for (let i=0;i<array.length;i++)
      {
-         SumWithoutMissingNo+=array[i];
+         SumWithoutMissingNo+=array[i];//sum=24
      }
-     let n=array.length+1;
-     let SumWithMissingNo=n*(n+1)/2;
-     let MissingNo=SumWithMissingNo-SumWithoutMissingNo;
+     let n=array.length+1;//n=6
+     let SumWithMissingNo=n*(n+1)/2;//sum=28
+     let MissingNo=SumWithMissingNo-SumWithoutMissingNo;//28-24=4
      res.send({ "Missing No" : MissingNo }); 
 });
 //assignment2(9-August):-"missing number of an array(interview question)"
@@ -115,11 +115,11 @@ router.get("/sol2",function(req,res){
     let SumWithoutMissingNo=0;
     for(let i=0;i<array.length;i++)
     {
-        SumWithoutMissingNo+=array[i];
+        SumWithoutMissingNo+=array[i];//sum=177
     }
-    let n=array.length+1;
-    let SumWithMissingNo=n*(array[0]+array[4])/2;
-    let MissingNo=SumWithMissingNo-SumWithoutMissingNo;
+    let n=array.length+1;//n=6
+    let SumWithMissingNo=n*(array[0]+array[4])/2;//sum=213
+    let MissingNo=SumWithMissingNo-SumWithoutMissingNo;//213-177=36
     res.send({"Missing No" : MissingNo})
 })
 
@@ -127,8 +127,164 @@ router.get('/test-you', function (req, res) {
     res.send('This is the second routes implementation')
 });
 
-router.get('/give-me-students-data', function (req, res) {
+// router.get('/give-me-students-data', functionpwd (req, res){
 
-})
+// });
+//Question:take input in a post request and add it to an array and return the new array
+router.post('/test-post-4',function(req,res){
+    let arr=[12,"functionup"];
+    let ele=req.body.element
+    arr.push(ele);
+    res.send({msg : arr ,status:true})
+});
+//assignment(10 august):Write a POST /players api that creates a new player ( i.e. that saves a player’s details and doesn’t allow saving the data of a player with a name that already exists in the data)
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+            let NewPlayer=req.body;
+            for(let i=0;i<players.length;i++)
+            {     let PlayerPerson=players[i];
+                if(PlayerPerson.name===NewPlayer.name)
+                {
+                    return res.send("Player is already existed");
+                }
+            }
+            players.push(NewPlayer);
+       
+       res.send(  { data: players , status: true }  )
+   })
+   //Write an api that books a slot for a player with relevant details. The api looks like POST /players/playerName/bookings/bookingId
+ 
+// Ensure the below conditions:
+// 1. PlayerName and bookingId are path params You have to ensure the playerName received must exist in the players collection. If the playerName doesn’t exist in the players collection return an error message that says something relevant about player not being found.	
+// 2. For a valid playerName check if the bookingId is already present in the player’s booking. Again, for a repeated bookingId send an error message conveying the booking was already processed. For a relevant bookingId(which is new), add the booking object from request body to bookings array
+
+
+     let players1 =
+     [
+         {
+             "name": "manish",
+             "bookingno":1,
+            "dob": "1/1/1995",
+             "gender": "male",
+             "city": "jalandhar",
+             "sports": [
+                 "swimming"
+             ]
+         },
+         {
+        
+             "name": "gopal",
+             "bookingno":2,
+             "dob": "1/09/1995",
+             "gender": "male",
+             "city": "delhi",
+             "sports": [
+                 "soccer"
+             ],
+         },
+        {
+        
+            "name": "lokesh",
+            "bookingno":3,
+             "dob": "1/1/1990",
+             "gender": "male",
+             "city": "mumbai",
+             "sports": [
+                 "soccer"
+             ],
+         },
+     ]
+     router.post('/players1', function (req, res) {
+        let Newplayer = req.body.name1;
+        let NewBookingid = req.body.bookingno1;
+        for (let i = 0; i < players1.length; i++) {
+            let people=players1[i];
+            if (people.name == Newplayer && people.bookingno == NewBookingid) {
+                return res.send({ Data: "Sorry! This Person is already exist." });
+            }
+        }
+        for(let i=0;i<players1.length;i++)
+        {
+            let people=players1[i];
+           if (people.name == Newplayer && people.bookingno != NewBookingid) {
+                return res.send({ Data: "Congratulations! Your New BookingId is created"});
+            }
+            else {
+                return res.send({ Data: "Sorry! No Match Found" });
+            }
+        }
+    
+    });   
+   //you will be given an array of persons(i.e an array of objects)..each person will have a {name:string,age:Number,votingStatus:true/false(Boolean)}take input in query param as votingAge..and for all the people above that age.change votingStatus as true also return an array consisting of only the person that can vote
+   //WRITE A POST API TO THE ABOVE
+   //take this as sample for array of persons:
+   let persons=[{
+    name:"PK",
+    age:10,
+    votingStatus:false,
+   },
+{
+    name:"SK",
+    age:20,
+    votingStatus:false
+},
+{
+    name:"AA",
+    age:70,
+    votingStatus:false
+},
+{
+    name:"SC",
+    age:5,
+    votingStatus:false
+},{
+    name:"HO",
+    age:40,
+    votingStatus:false
+}
+] 
+router.post('/persons', function (req, res){
+     let VotingAge=req.query.VotingAge;
+     let NewPerson=[];
+     for(let i =0;i<persons.length;i++){
+        let people=persons[i];
+         if(people.age>VotingAge)
+         {
+            people.votingStatus=true;
+            NewPerson.push(people);
+        } 
+    }
+    return res.send({Data:NewPerson,status:true})
+});
 module.exports = router;
 // adding this comment for no reason
